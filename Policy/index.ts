438c2cde450b9a7a05411fa867b8731f21807699
Abdi Hassan <contact@justbeingabdi.cf>
2021-04-03 23:16:44 +0200

    + Policy

---
+++ Policy/index.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import path from "path";
+import bodyparser from "body-parser";
+import ejs from "ejs";
+import { ServerConfig } from "./../lib";
+
+export class Server {
+    public async init(port): Promise<any> {
+       
+        let app = express();
+        
+
+    app.use(bodyparser.json());
+    app.use(log.express());
+    app.use(bodyparser.urlencoded({ extended: true }));
+    app.engine("html", ejs.renderFile);
+    app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname, "./../views/policy"));
+    
+    app.set('trust proxy', true);
+    
+
+
+    app.get("/", async(req, res) => {
+        res.render("index", {
+            config: ServerConfig
+        });
+    });
+    app.get("/reset", async(req, res) => {
+        res.render("reset");
+    })
+    
+
+    app.listen('5757');
+        console.log('Webserver online on port 5757');
+
+
+
+    }
+}
